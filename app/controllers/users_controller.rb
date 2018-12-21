b6d1f59450c10a51c35f01ef87aa0cabd0d30328
NathanB09 <nathanbell09@gmail.com>
2018-12-21 14:19:58 +0000

    added actions to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit()
+  end
 end
