408d60f5c12288c9b91c299e8048251fbde8518d
NathanB09 <nathanbell09@gmail.com>
2018-12-21 14:30:27 +0000

    added actions to exercises_controller

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,2 +1,22 @@
 class ExercisesController < ApplicationController
+  def index
+    @exercises = Exercise.all
+  end
+
+  def show
+    @exercise = Exercise.find(params[:id])
+  end
+
+  def new
+    @exercise = Exercise.new
+  end
+
+  def create
+  end
+
+  private
+
+  def exercise_params
+    params.require(:exercise).permit()
+  end
 end
