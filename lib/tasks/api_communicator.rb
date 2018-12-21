d80eb4d7ba12b2c68b322aef59b77d4d3ce212f2
NathanB09 <nathanbell09@gmail.com>
2018-12-21 16:06:57 +0000

    removed google api key

--- lib/tasks/api_communicator.rb
+++ lib/tasks/api_communicator.rb
@@ -2,7 +2,7 @@ require 'rest-client'
 require 'JSON'
 
 def get_data_from_api_gyms_nearby_search(location) #NearbySearch! 1500m radius; location must be specified as latitude,longtude
-  response_string = RestClient.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + location + "&radius=1500&type=gym&rankby=prominence&key=AIzaSyCRy-gJvn2d98IQU6CYLc6kMfisMl5M-bM")
+  response_string = RestClient.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + location + "&radius=1500&type=gym&rankby=prominence&key=")
   response_hash = JSON.parse(response_string)
   gyms_hash = {}
   response_hash["results"].each do |hash|
@@ -14,7 +14,7 @@ end
 #get_data_from_api_gyms_nearby_search("51.520341,-0.087603")
 
 def get_data_from_api_gyms_find_nearest_place(location) #Find Place gives 1 response only! This is the nearest thing
-  response_string = RestClient.get("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=gym&locationbias=circle:500@" + location + "&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyCRy-gJvn2d98IQU6CYLc6kMfisMl5M-bM")
+  response_string = RestClient.get("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=gym&locationbias=circle:500@" + location + "&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=")
   response_hash = JSON.parse(response_string)
   gyms_hash = {}
   response_hash["candidates"].each do |gym|
