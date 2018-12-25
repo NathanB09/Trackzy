ce9a041d6414e3a61bf437be3d1b00f368707285
NathanB09 <nathanbell09@gmail.com>
2018-12-25 19:44:12 +0000

    added username and password columns for login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :workouts
   has_many :gyms, through: :workouts
   has_many :exercises, through: :workouts
+  has_secure_password
 end
--- db/migrate/20181221132257_create_users.rb
+++ db/migrate/20181221132257_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
+      t.string :email
+      t.string :password_digest
       t.integer :height_cm
       t.integer :weight_kg
 
