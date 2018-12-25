73aac0dc12ea1c78bc3df79c06ae29195332dff1
NathanB09 <nathanbell09@gmail.com>
2018-12-25 19:26:17 +0000

    created routes for login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'application#home'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
   resources :exercises
   resources :workout_excercises
   resources :gyms
