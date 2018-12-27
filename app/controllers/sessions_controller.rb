2fe59c94ca70926c8bc7248bbd536d011692264d
NathanB09 <nathanbell09@gmail.com>
2018-12-27 16:26:52 +0000

    added login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      flash[:errors] = ["Invalid email or password"]
+      redirect_to login_path
+    end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:notice] = "You have successfully logged out"
+    redirect_to root_path
   end
 end
