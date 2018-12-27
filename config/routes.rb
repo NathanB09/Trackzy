cf10157c0e02aa9cddb3311b52a0ba92e63af536
NathanB09 <nathanbell09@gmail.com>
2018-12-27 16:31:18 +0000

    edited some of the users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
   root 'application#home'
+
+  # sessions management
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
+
+  # users management
+  get '/signup', to: 'users#new', as: 'signup'
+  post '/signup', to: 'users#create'
+  resources :users, only: [:new, :create, :show]
+
   resources :exercises
   resources :workout_excercises
   resources :gyms
   resources :workouts
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
