6d2c6af0fbb9ccd6e65a9193f314a929bdc68097
NathanB09 <nathanbell09@gmail.com>
2018-12-30 21:42:26 +0000

    added controller functionality to render new form and create workouts with multiple exercises

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,6 +1,7 @@
 class Workout < ApplicationRecord
   belongs_to :user
-  belongs_to :gym
-  has_many :workout_exercises
+  # belongs_to :gym
+  has_many :workout_exercises, inverse_of: :workout
   has_many :exercises, through: :workout_exercises
+  accepts_nested_attributes_for :workout_exercises
 end
