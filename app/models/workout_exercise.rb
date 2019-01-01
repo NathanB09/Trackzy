d192f1e0aae9b88b126bbccc5e030b0699e6e8f8
NathanB09 <nathanbell09@gmail.com>
2019-01-01 18:31:28 +0000

    validates the exercise inputs have been filled in

--- app/models/workout_exercise.rb
+++ app/models/workout_exercise.rb
@@ -1,5 +1,5 @@
 class WorkoutExercise < ApplicationRecord
   belongs_to :workout
   belongs_to :exercise
-  accepts_nested_attributes_for :exercise, allow_destroy: true
+  accepts_nested_attributes_for :exercise, allow_destroy: true, reject_if: proc { |attr| attr['name'].blank? && attr['sets'].blank? && attr['reps'].blank? }
 end
